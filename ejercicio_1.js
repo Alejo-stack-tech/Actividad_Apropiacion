6e18005f386dce9fd266d73967d0eb71445a508f
XimenaRubio212 <ximenarubio121@gmail.com>
2025-09-29 20:55:21 -0500

    2ejercicio.js

---
+++ ejercicio_1.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingrese su nombre");
+
+alert("nombre")
+
+let edad = prompt("Ingrese su edad");
+
+alert("edad")
+
+console.log("Hola " + nombre + " tienes " + edad + " años");
---
+++ ejercicio_2.js
@@ -0,0 +1,8 @@
+let Base = prompt("Ingrese la base del rectángulo");
+alert("Base")
+
+let Altura = prompt("Ingrese la altura del rectángulo");
+alert("Altura")
+let Area = Base * Altura;
+
+alert("El área del rectángulo es: " + Area);
